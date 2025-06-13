667b82977b327c0724faab99221681f4da813c77
tushar-more-bbi <tushar.more@media4u.com>
2025-06-13 09:58:02 +0530

    Add Functionality for Login as Guest

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -41,8 +41,27 @@ export const Login = () => {
         closeButton: true,
       })
     }
+  }
 
-  
+  async function handleGuestLogin(){
+    
+    const authDetails = {
+      email: import.meta.env.VITE_APP_GUEST_LOGIN,
+      password: import.meta.env.VITE_APP_GUEST_PASSWORD,
+    }
+
+    console.log(authDetails.email, authDetails.password);
+
+    try{
+      const data = await loginUser(authDetails);
+      data.accessToken ? navigate('/products') : toast.error(data);
+    }
+    catch(error) {
+      toast.error(error.message, {
+        autoClose: 5000,
+        closeButton: true,
+      })
+    }
   }
 
 
@@ -62,7 +81,7 @@ export const Login = () => {
         </div>
         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
       </form>
-      {/* <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> */}
+      <button onClick={handleGuestLogin}className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> 
     </main>
   )
 }
