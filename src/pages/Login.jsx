28f79686e53e3e677f72ce68bde5158d528c1fe8
tushar-more-bbi <tushar.more@media4u.com>
2025-06-13 10:51:35 +0530

    Fixed Guest Login Bugs

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,15 +26,15 @@ export const Login = () => {
     // })
     // const data = await response.json();
     // console.log(data);
-      // if(data.accessToken){
+    // if(data.accessToken){
     //   sessionStorage.setItem('token', data.accessToken);
     //   sessionStorage.setItem('id', data.user.id);
     //   sessionStorage.setItem('name', data.user.name);
     // }
-    try{
+    try {
       const data = await loginUser(authDetails)
       data.accessToken ? navigate('/products') : toast.error(data);
-    }  
+    }
     catch (error) {
       toast.error(error.message, {
         autoClose: 5000,
@@ -43,8 +43,8 @@ export const Login = () => {
     }
   }
 
-  async function handleGuestLogin(){
-    
+  async function handleGuestLogin() {
+
     const authDetails = {
       email: import.meta.env.VITE_APP_GUEST_LOGIN,
       password: import.meta.env.VITE_APP_GUEST_PASSWORD,
@@ -52,11 +52,11 @@ export const Login = () => {
 
     // console.log(authDetails.email, authDetails.password);
 
-    try{
+    try {
       const data = await loginUser(authDetails);
       data.accessToken ? navigate('/products') : toast.error(data);
     }
-    catch(error) {
+    catch (error) {
       toast.error(error.message, {
         autoClose: 5000,
         closeButton: true,
@@ -81,7 +81,7 @@ export const Login = () => {
         </div>
         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
       </form>
-      <button onClick={handleGuestLogin}className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> 
+      <button onClick={handleGuestLogin} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
     </main>
   )
 }
